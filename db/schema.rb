89da3c16b12cf3429a46129160882303ceb3bbd8
Paul Tevis <paul.tevis@appfolio.com>
2017-07-13 16:12:59 -0700

    Replace out-of-date schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,14 +16,6 @@ ActiveRecord::Schema.define(version: 20160511004729) do
     t.string   "name"
     t.string   "gender"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "people", force: :cascade do |t|
-    t.integer  "age"
-    t.string   "gender"
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
