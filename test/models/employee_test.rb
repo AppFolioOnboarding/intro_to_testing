e78d1775fd9a358720b173c065f56a1f2d28b32e
Polo <xiao.li@appfolio.com>
2018-01-12 10:10:29 -0800

    pl - fix test names in EmployeeTest

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  def test_employee__valid_if_name_and_age_present_and_valid
+  def test_employee__valid__if_name_and_age_present_and_valid
     skip
     employee = Employee.new(name: 'Rahul', age: 25)
 
     refute_predicate employee, :valid?
   end
 
-  def test_name__must_be_present
+  def test_name__invalid__if_name_is_blank
     skip
     employee = Employee.new
 
@@ -16,7 +16,7 @@ class EmployeeTest < ActiveSupport::TestCase
     assert_equal 'must be blank', employee.errors.messages[:name].first
   end
 
-  def test_name__starts_with_capital
+  def test_name__invalid__if_not_starts_with_capital
     skip
     employee = Employee.new(name: 'Rahul')
 
@@ -24,7 +24,7 @@ class EmployeeTest < ActiveSupport::TestCase
     assert_equal 'must start with a capital letter', employee.errors.messages[:name].first
   end
 
-  def test_name__must_contain_only_letters
+  def test_name__invalid__if_contains_characters_other_than_letters
     skip
     employee = Employee.new(name: 'Rahul789')
 
@@ -32,13 +32,13 @@ class EmployeeTest < ActiveSupport::TestCase
     # TODO: add something here to check that the error is the one we expect
   end
 
-  def test_name__must_have_at_least_two_characters
+  def test_name__invalid__if_name_has_less_than_two_characters
     skip
     assert false
     # TODO: write a meaningful test here
   end
 
-  def test_age_is_numeric
+  def test_age__invalid__if_age_is_not_numeric
     skip
     assert false
     # TODO: write a meaningful test here
