90524d8e258087d6fedc501f17f3c9b17c912b9d
Polo <xiao.li@appfolio.com>
2018-02-22 16:58:16 -0800

    pl - change Employee model only has name and age

--- db/migrate/20160511004729_create_employees.rb
+++ db/migrate/20160511004729_create_employees.rb
@@ -2,7 +2,6 @@ class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
       t.string :name
-      t.string :gender
       t.integer :age
 
       t.timestamps null: false
