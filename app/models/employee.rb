c5652cd75d346ec596894f4d379921e6ec1f49ec
Polo <xiao.li@appfolio.com>
2018-02-22 17:24:18 -0800

    pl - make employee model test more organizable

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,29 +1,11 @@
 class Employee < ApplicationRecord
   validates :name, presence: true
-  validate :name_is_valid
+  validate :name_must_start_with_uppercase
   validates :age, numericality: true
-  validate :gender_is_valid
 
   private
 
-  def name_is_valid
-    return if name.blank?
-
-    name_starts_with_uppercase
-    name_only_contains_letters_and_is_long_enough
-  end
-
-  def name_starts_with_uppercase
+  def name_must_start_with_uppercase
     errors.add(:name, 'must start with a capital letter') unless name.capitalize == name
-  end
-
-  NAME_ERROR_MESSAGE = 'must be made of letters only and be at least 2 characters long'.freeze
-
-  def name_only_contains_letters_and_is_long_enough
-    errors.add(:name, NAME_ERROR_MESSAGE) unless /^[a-zA-Z]{2,}$/.match?(name)
-  end
-
-  def gender_is_valid
-    errors.add(:gender, "must be 'F' or 'M'") unless gender.nil? || %w[F M].include?(gender)
   end
 end
--- solutions/employee_test.rb
+++ solutions/employee_test.rb
@@ -1,53 +1,31 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  def test_employee__valid__if_name_and_age_present_and_valid
-    employee = Employee.new(name: 'Rahul', age: 25)
+
+  def test_employee__valid
+    employee = Employee.new(name: 'God', age: 10)
 
     assert_predicate employee, :valid?
   end
 
-  def test_name__invalid__if_name_is_blank
-    employee = Employee.new
+  def test_name__invalid_if_name_is_blank
+    employee = Employee.new(name: '', age: 10)
 
     refute_predicate employee, :valid?
     assert_equal "can't be blank", employee.errors.messages[:name].first
   end
 
-  def test_name__invalid__if_not_starts_with_capital
-    employee = Employee.new(name: 'rahul')
+  def test_name__invalid_if_name_does_not_start_with_uppercase
+    employee = Employee.new(name: 'god', age: 10)
 
     refute_predicate employee, :valid?
     assert_equal 'must start with a capital letter', employee.errors.messages[:name].first
   end
 
-  NAME_ERROR_MESSAGE = 'must be made of letters only and be at least 2 characters long'.freeze
-
-  def test_name__invalid__if_contains_characters_other_than_letters
-    employee = Employee.new(name: 'Rahul789')
-
-    refute_predicate employee, :valid?
-    assert_equal NAME_ERROR_MESSAGE, employee.errors.messages[:name].first
-  end
-
-  def test_name__invalid__if_name_has_less_than_two_characters
-    employee = Employee.new(name: 'R')
+  def test_age__invalid_if_age_is_not_a_number
+    employee = Employee.new(name: 'God', age: 'ten')
 
     refute_predicate employee, :valid?
     assert_equal 'is not a number', employee.errors.messages[:age].first
-  end
-
-  def test_age__invalid__if_age_is_not_numeric
-    employee = Employee.new(name: 'Rahul', age: 'fifty')
-
-    refute_predicate employee, :valid?
-    assert_equal 'is not a number', employee.errors.messages[:age].first
-  end
-
-  def test_gender_is_valid
-    employee = Employee.new(gender: 'male')
-
-    refute_predicate employee, :valid?
-    assert_equal "must be 'F' or 'M'", employee.errors.messages[:gender].first
   end
 end
